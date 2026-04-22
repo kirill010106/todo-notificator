f62057112097bc7049108ae0aa648afd5d1fe38b
Guitaramust <guitaramust@gmail.com>
2026-04-22 11:47:12 +0300

    mobx store for auth + tanstack for auth
    More staleTime for Auth request

--- frontend-churka/todo-notificator/src/App.tsx
+++ frontend-churka/todo-notificator/src/App.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
+import { QueryClientProvider } from "@tanstack/react-query";
+import queryClient from "./queryClient/queryClient";
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Outlet />
-    </>
+    </QueryClientProvider>
   );
 }
 
--- frontend-churka/todo-notificator/src/pages/MainPage/MainPage.tsx
+++ frontend-churka/todo-notificator/src/pages/MainPage/MainPage.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useState, useEffect, useCallback, useMemo } from "react";
 import TaskHeader from "../../components/TaskHeader/TaskHeader";
 import TaskForm from "../../components/TaskForm/TaskForm";
